e5cdd6d1e55fc8d5baf0745e4cbfd201ecad1d8b
ASUS\diego <diegoacosta9239@gmail.com>
2023-12-07 20:23:23 -0400

    v1

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyCkxxEgtDtW7Zl_LpIWFYQ-9Hi7Cmjsekw",
+    authDomain: "caso-venta.firebaseapp.com",
+    projectId: "caso-venta",
+    storageBucket: "caso-venta.appspot.com",
+    messagingSenderId: "894654326330",
+    appId: "1:894654326330:web:b71dad9d3d0f08f35bb703"
+  };
